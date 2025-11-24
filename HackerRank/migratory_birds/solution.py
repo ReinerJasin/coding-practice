ca1be1099114d6cfb08316dacef2a105cdad2e2f
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2025-11-24 17:46:36 +0800

    solution: migratory birds

---
+++ HackerRank/migratory_birds/solution.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    count = {}
+    
+    for i in sorted(arr):
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
+            
+    for key, value in count.items():
+        if value == max(count.values()):
+            return key
+            
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
