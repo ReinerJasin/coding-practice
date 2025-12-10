0caae9b39647bc527102111164eb648d1d11b397
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2025-12-10 16:39:49 +0800

    Solution: picking numbers

---
+++ HackerRank/.template_folder copy/solution.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    # Write your code here
+    max_counter = 0
+    
+    for number in a:
+        filtered = list(filter(lambda x: (x == number or x == number+1), a))
+        
+        if len(filtered) > max_counter:
+            max_counter = len(filtered)
+    
+    return max_counter
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
