49c7e58c5d5da97f232cf29c57ce478e5219cd1d
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2025-12-22 00:22:20 +0800

    solution: designer pdf viewer

---
+++ HackerRank/designer_pdf_viewer/solution.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Write your code here
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    word_num = []
+    
+    # Find the corresponding number for each word
+    for i in word.lower():
+        word_num.append(h[alphabet.index(i)])
+    
+    # max height * num of words
+    return max(word_num) * len(word_num)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
